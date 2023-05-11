05284be8c3228ffe73a26d97141d2d488adae594
DeXtreme <dbakolbire@gmail.com>
2023-05-11 17:50:12 +0000

    This is our first commit

---
+++ calculator.py
@@ -0,0 +1,20 @@
+def add(x,y):
+	return x+y
+
+def subtract(x,y):
+	return x-y
+
+def mult(x,y):
+	return x*y
+
+def div(x,y):
+	return x/y
+
+
+x=1
+y=2
+
+print(add(x,y))
+print(subtract(x,y))
+print(mult(x,y))
+print(div(x,y))
